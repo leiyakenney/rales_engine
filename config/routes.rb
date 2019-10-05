1767bbec54ab100fa1e6aa8168ee84acf2392098
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-05 15:38:15 -0600

    Creates routes for index/show

--- config/routes.rb
+++ config/routes.rb
@@ -2,37 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, except: [:new, :edit]
+      resources :merchants, only: [:index, :show]
+      resources :customers, only: [:index, :show]
+      resources :invoice_items, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
+      resources :items, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
     end
   end
 
-  namespace :api do
-    namespace :v1 do
-      resources :customers, except: [:new, :edit]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :invoice_items, except: [:new, :edit]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :invoices, except: [:new, :edit]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :items, except: [:new, :edit]
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :transactions, except: [:new, :edit]
-    end
-  end
 end
