07211b3976fa1e43c148d9b25a61153dc3889ae9
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-06 10:51:16 -0600

    Adds search show/index paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+      end
+
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
@@ -10,5 +15,4 @@ Rails.application.routes.draw do
       resources :transactions, only: [:index, :show]
     end
   end
-
 end
