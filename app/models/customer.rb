89c64d8de890c7643fae9eb0956dedd688961288
Leiya Kenney <leiya.kenney@gmail.com>
2019-10-06 14:21:01 -0600

    Updates relationships, adds favorite_customer method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,14 @@
 class Customer < ApplicationRecord
-  has_many :transactions
   has_many :invoices
 
-  def fav_merchant
-
+  def self.favorite_customer(merchant_id)
+    joins(invoices:[:merchant, :transactions]).
+    select("customers.*, COUNT(*) AS cust").
+    group(:id).
+    where(invoices:{merchant_id: merchant_id}).
+    merge(Transaction.successful).
+    order("cust DESC").
+    limit(1)
   end
 
 end
